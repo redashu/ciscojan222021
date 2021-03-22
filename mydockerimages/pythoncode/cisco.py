8abe33dee359ac8ac7eeefaaf17899c9a3ac3e6b
Ashutoshh <asashuthegreat4@gmail.com>
2021-03-22 13:15:17 +0530

    Add files via upload

---
+++ mydockerimages/pythoncode/cisco.py
@@ -0,0 +1,8 @@
+import time
+while 3 > 2:
+    print("Hello CIsco !!")
+    time.sleep(2)
+    print("sample python code to containerize !!")
+    time.sleep(1)
+    print("_______________")
+    time.sleep(2)
